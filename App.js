4758e487313f9ec0e2456b83c53a52252c937223
pedrogarcia2111 <48315562@est.ort.edu.ar>
2025-04-28 22:12:55 -0300

    Primer commit

---
+++ App.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { StyleSheet, View, Text, Button, ImageBackground, TouchableOpacity  } from 'react-native';
+
+function Contador() {
+  const [contador, setContador] = useState(0);
+
+  function Sumar() {
+    setContador(contador + 1);
+  }
+  function Restar() {
+    setContador(contador - 1);
+  }
+
+  function Reiniciar() {
+    setContador(0);
+  }
+
+
+  return (
+    <View style={styles.contenedor}>
+      
+      <Text style={styles.contador}>Contador: {contador}</Text>
+
+      
+        <TouchableOpacity style={styles.boton} onPress={Sumar}>
+          <Text style={styles.textoboton}>Sumar</Text>
+        </TouchableOpacity>
+      
+
+      
+        <TouchableOpacity style={styles.boton} onPress={Restar}>
+          <Text style={styles.textoboton}>Restar</Text>
+        </TouchableOpacity>
+      
+
+      
+        <TouchableOpacity style={styles.boton} onPress={Reiniciar}>
+          <Text style={styles.textoboton}>Reiniciar</Text>
+        </TouchableOpacity>
+      
+
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  contenedor: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  contador: {
+    fontSize: 40,
+    marginBottom: 25,
+    fontWeight: '600'
+  },
+  boton: {
+    alignItems: 'center',
+    backgroundColor: '#0080ff',
+    padding: 10,
+    marginTop: 35,
+    width: 170,
+    borderRadius: 7
+  },
+  textoboton: {
+    fontSize: 20,
+    color: 'white'
+  }
+  });
+  
+export default Contador;
